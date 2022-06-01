111da765a2804e34395e965f26deec433f48d4f6
Victor Profir <victor.profir@gdm.com>
2022-06-01 18:53:12 +0300

    commit

--- index.js
+++ index.js
@@ -16,57 +16,69 @@ function findCenterOf(element) {
   return center;
 }
 function segmentCheck(angle) {
-  if (angle < 22.5) return 1;
-  if (angle == 337.5) return 1;
-  let k = 0;
-  let aux = 400;
-  let segmentMiddles = [360, 45, 90, 135, 180, 225, 270, 315];
-  for (let i = 0; i < segmentMiddles.length; i++) {
-    if (Math.abs(angle - segmentMiddles[i]) <= aux) {
-      aux = Math.abs(angle - segmentMiddles[i]);
-      k = i;
-    }
-  }
-  return k + 1;
+  if (22.5 < angle && angle < 337.5) {
+    return Math.round(angle / 45) + 1;
+  } else return 1;
 }
+let i = 0;
+let actualLastZ=9;
+let lastZ=9;
+let z;
+
 function miscaFete() {
-  for (let i = 0; i < arrOfCenters.length; i++) {
-    let img = document.querySelector(`.grid-item:nth-of-type(${i + 1}) img`);
-    let angleBetweenMouseAndCenter = findAngle(
-      arrOfCenters[i].centerX,
-      arrOfCenters[i].centerY,
-      pointerX,
-      pointerY
+  // for (let i = 0; i < arrOfCenters.length; i++) {
+    let currentImg = document.querySelector(
+      `.grid-item:nth-of-type(${i + 1}) img:nth-of-type(1) `
     );
-    let z = segmentCheck(angleBetweenMouseAndCenter);
-    let x = Math.floor(i / 4);
-    let y = i % 4;
-    if (`${x}${y}` == currContainer) {
-      img.src = `./imgs/${x}${y}9.jpg`;
-    } else {
-      img.src = `./imgs/${x}${y}${z}.jpg`;
-    }
+    let futureImg = document.querySelector(
+      `.grid-item:nth-of-type(${i + 1}) img:nth-of-type(2) `
+    );
+  let angleBetweenMouseAndCenter = findAngle(
+    arrOfCenters[i].centerX,
+    arrOfCenters[i].centerY,
+    pointerX,
+    pointerY
+  );
+  let x = Math.floor(i / 4);
+  let y = i % 4;
+  if (`${x}${y}` == currContainer) {
+    z = 9;
+  } else z = segmentCheck(angleBetweenMouseAndCenter);
+
+  if (lastZ != z) {
+    actualLastZ = lastZ
+    currentImg.src = `./imgs/${x}${y}${actualLastZ}.jpg`;
+    futureImg.src = `./imgs/${x}${y}${z}.jpg`;
+    currentImg.classList.toggle('show')
+    // setTimeout(() => {
+    //   currentImg.classList.remove('show')
+    // }, 500);
   }
+
+  lastZ = z;
+
+  console.log(`${z}     ${lastZ}    ${actualLastZ}`)
+  // }
 }
 let pointerX = -1;
 let pointerY = -1;
 const imgContainers = document.querySelectorAll(".grid-item");
 let currContainer;
 const arrOfCenters = [];
-let intervalID;
-
+let regex = "./imgs/([0-9]{3})";
+let aux;
 for (let imgContainer of imgContainers) {
   arrOfCenters.push(findCenterOf(imgContainer));
   imgContainer.addEventListener("mouseenter", (e) => {
-    currContainer = e.target.innerHTML.slice(41, 43);
+    const match = e.target.children[0].currentSrc.match(regex);
+    currContainer = match[1].slice(0, 2); //luam primele 2 cifre din poza, care ne dau randul si coloana
   });
   imgContainer.addEventListener("mouseleave", () => {
     currContainer = -1;
   });
 }
-
 document.onmousemove = function (event) {
   pointerX = event.pageX;
   pointerY = event.pageY;
-  miscaFete()
+  miscaFete();
 };
